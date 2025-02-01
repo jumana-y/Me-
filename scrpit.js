7a89395c5dcdd9812b4eb72d5fe66f9f7d636bd0
Jumana <jumanajumi693@gmail.com>
2025-02-01 15:51:22 -0800

    added content

--- scrpit.js
+++ scrpit.js
@@ -15,7 +15,47 @@ const productsData = {
             weight: ["500g", "1kg"],
             price: {"500g": "₹449", "1kg": "₹799"},
             shops: ["Royal Bakes", "Golden Sweet"]
-        }
+        },
+        {
+            name: "vancho",
+            image: "image/vancho.jpg",
+            specs: "Classic red velvet with cream cheese frosting",
+            weight: ["500g", "1kg"],
+            price: {"500g": "₹449", "1kg": "₹799"},
+            shops: ["Royal Bakes", "Golden Sweet"]
+        },
+        {
+            name: "plum cake",
+            image: "image/plum cake.jpg",
+            specs: "Classic red velvet with cream cheese frosting",
+            weight: ["500g", "1kg"],
+            price: {"500g": "₹449", "1kg": "₹799"},
+            shops: ["Royal Bakes", "Golden Sweet"]
+        },
+        {
+            name: "caramel cake",
+            image: "image/caramel cake.jpg",
+            specs: "Classic red velvet with cream cheese frosting",
+            weight: ["500g", "1kg"],
+            price: {"500g": "₹449", "1kg": "₹799"},
+            shops: ["Royal Bakes", "Golden Sweet"]
+        },
+        {
+            name: "milk cake",
+            image: "image/vanila.jpg",
+            specs: "Classic red velvet with cream cheese frosting",
+            weight: ["500g", "1kg"],
+            price: {"500g": "₹449", "1kg": "₹799"},
+            shops: ["Royal Bakes", "Golden Sweet"]
+        },
+        {
+            name: "pista choco cake",
+            image: "image/pista choco cake.jpg",
+            specs: "Classic red velvet with cream cheese frosting",
+            weight: ["500g", "1kg"],
+            price: {"500g": "₹449", "1kg": "₹799"},
+            shops: ["Royal Bakes", "Golden Sweet"]
+        },
     ],
     fruits: [
         {
@@ -33,12 +73,60 @@ const productsData = {
             weight: ["1kg", "2kg"],
             price: {"1kg": "₹129", "2kg": "₹249"},
             shops: ["Orchard Bliss", "The Fruity Stop"]
-        }
+        },
+        {
+            name: "strawberry",
+            image: "image/strawberry.jpg",
+            specs: "Nagpur Oranges",
+            weight: ["1kg", "2kg"],
+            price: {"1kg": "₹129", "2kg": "₹249"},
+            shops: ["Orchard Bliss", "The Fruity Stop"]
+        },
+        {
+            name: "avocado",
+            image: "image/avocado.jpg",
+            specs: "Nagpur Oranges",
+            weight: ["1kg", "2kg"],
+            price: {"1kg": "₹129", "2kg": "₹249"},
+            shops: ["Orchard Bliss", "The Fruity Stop"]
+        },
+        {
+            name: "green grapes",
+            image: "image/green grapes.jpg",
+            specs: "Nagpur Oranges",
+            weight: ["1kg", "2kg"],
+            price: {"1kg": "₹129", "2kg": "₹249"},
+            shops: ["Orchard Bliss", "The Fruity Stop"]
+        },
+        {
+            name: "pineaple",
+            image: "image/pineaple.jpg",
+            specs: "Nagpur Oranges",
+            weight: ["1kg", "2kg"],
+            price: {"1kg": "₹129", "2kg": "₹249"},
+            shops: ["Orchard Bliss", "The Fruity Stop"]
+        },
     ],
     grocery: [
         {
             name: "Organic Milk",
             image: "image/organic milk.jpg",
+            specs: "Fresh farm milk, pasteurized",
+            weight: ["500ml", "1L"],
+            price: {"500ml": "₹35", "1L": "₹65"},
+            shops: ["FMS Mart", "GDS"]
+        },
+        {
+            name: "soft drinks",
+            image: "image/soft drinks.jpg",
+            specs: "Fresh farm milk, pasteurized",
+            weight: ["500ml", "1L"],
+            price: {"500ml": "₹35", "1L": "₹65"},
+            shops: ["FMS Mart", "GDS"]
+        },
+        {
+            name: "vegitable",
+            image: "image/vegitable.jpg",
             specs: "Fresh farm milk, pasteurized",
             weight: ["500ml", "1L"],
             price: {"500ml": "₹35", "1L": "₹65"},
@@ -53,7 +141,39 @@ const productsData = {
             weight: ["Basic", "Premium"],
             price: {"Basic": "₹499", "Premium": "₹999"},
             shops: ["MedPlus", "Apollo Pharmacy"]
-        }
+        },
+        {
+            name: "glutation",
+            image: "image/glutation.jpg",
+            specs: "Complete emergency medical kit",
+            weight: ["Basic", "Premium"],
+            price: {"Basic": "₹499", "Premium": "₹999"},
+            shops: ["MedPlus", "Apollo Pharmacy"]
+        },
+        {
+            name: "paracetamol",
+            image: "image/paracetamol.jpg",
+            specs: "Complete emergency medical kit",
+            weight: ["Basic", "Premium"],
+            price: {"Basic": "₹499", "Premium": "₹999"},
+            shops: ["MedPlus", "Apollo Pharmacy"]
+        },
+        {
+            name: "vitaminD3 tablet",
+            image: "image/vitaminD3 tablet.jpg",
+            specs: "Complete emergency medical kit",
+            weight: ["Basic", "Premium"],
+            price: {"Basic": "₹499", "Premium": "₹999"},
+            shops: ["MedPlus", "Apollo Pharmacy"]
+        },
+        {
+            name: "vitamin tablet",
+            image: "image/vitamin tablet.jpg",
+            specs: "Complete emergency medical kit",
+            weight: ["Basic", "Premium"],
+            price: {"Basic": "₹499", "Premium": "₹999"},
+            shops: ["MedPlus", "Apollo Pharmacy"]
+        },
     ],
     study_material: [
         {
@@ -63,8 +183,232 @@ const productsData = {
             weight: ["Standard", "Premium"],
             price: {"Standard": "₹299", "Premium": "₹599"},
             shops: ["Academic Books", "Scholar's Corner"]
-        }
-    ]
+        },
+        {
+            name: "pens",
+            image: "image/pens.jpg",
+            specs: "Includes notebooks, pens, and basic stationery",
+            weight: ["Standard", "Premium"],
+            price: {"Standard": "₹299", "Premium": "₹599"},
+            shops: ["Academic Books", "Scholar's Corner"]
+        },
+        {
+            name: "pencile",
+            image: "image/pencile.jpg",
+            specs: "Includes notebooks, pens, and basic stationery",
+            weight: ["Standard", "Premium"],
+            price: {"Standard": "₹299", "Premium": "₹599"},
+            shops: ["Academic Books", "Scholar's Corner"]
+        },
+        
+    ],
+    dresess: [
+        {
+            name: "skirt",
+            image: "image/skirt.jpg",
+            specs: "Includes top,pant and basic stationery",
+            weight: ["Standard", "Premium"],
+            price: {"Standard": "₹299", "Premium": "₹599"},
+            shops: ["dana", "sinus"]
+        },
+        {
+            name: "kurtha",
+            image: "image/kurtha set.jpg",
+            specs: "Includes belt, and basic stationery",
+            weight: ["Standard", "Premium"],
+            price: {"Standard": "₹299", "Premium": "₹499"},
+            shops: ["luna", "abc shop"]
+        },
+        {
+            name: "code set",
+            image: "image/code set.jpg",
+            specs: "Includes pant top and basic stationery",
+            weight: ["Standard", "Premium"],
+            price: {"Standard": "₹80", "Premium": "₹160"},
+            shops: ["lulu", "abc shop"]
+        },
+        {
+            name: "jacket",
+            image: "image/jaket.jpg",
+            specs: "Includes specs, and basic stationery",
+            weight: ["Standard", "Premium"],
+            price: {"Standard": "₹599", "Premium": "₹999"},
+            shops: ["zudip", "abc shop"]
+        },
+     ],
+    readyfood: [
+            {
+                name: "pizza",
+                image: "image/pizza.jpg",
+                specs: "Includes top,pant and basic stationery",
+                weight: ["Standard", "Premium"],
+                price: {"Standard": "₹299", "Premium": "₹599"},
+                shops: ["dana", "sinus"]
+            },
+            {
+                name: "burger",
+                image: "image/burger.jpg",
+                specs: "Includes belt, and basic stationery",
+                weight: ["Standard", "Premium"],
+                price: {"Standard": "₹60", "Premium": "₹180"},
+                shops: ["luna", "abc shop"]
+            },
+            {
+                name: "biriyani",
+                image: "image/biriyani.jpg",
+                specs: "Includes pant top and basic stationery",
+                weight: ["Standard", "Premium"],
+                price: {"Standard": "₹80", "Premium": "₹160"},
+                shops: ["lulu", "abc shop"]
+            },
+            {
+                name: "unniyappam",
+                image: "image/unniyappam.jpg",
+                specs: "Includes specs, and basic stationery",
+                weight: ["Standard", "Premium"],
+                price: {"Standard": "₹10", "Premium": "₹15"},
+                shops: ["fms", "ramettante kada"]
+            },
+         ],
+         sportsitems: [
+            {
+                name: "boot",
+                image: "image/boots.jpg",
+                specs: "Includes top,pant and basic stationery",
+                weight: ["Standard", "Premium"],
+                price: {"Standard": "₹115", "Premium": "₹5000"},
+                shops: ["sports shop", "abc sports"]
+            },
+            {
+                name: "jersey",
+                image: "image/jersey.jpg",
+                specs: "Includes belt, and basic stationery",
+                weight: ["Standard", "Premium"],
+                price: {"Standard": "₹150", "Premium": "₹500"},
+                shops: ["sports shop", "abc shop"]
+            },
+            {
+                name: "boxing glove",
+                image: "image/boxing.jpg",
+                specs: "Includes pant top and basic stationery",
+                weight: ["Standard", "Premium"],
+                price: {"Standard": "₹600", "Premium": "₹1500"},
+                shops: ["lulu", "abc shop"]
+            },
+            
+         ],
+         jwellery: [
+            {
+                name: "necklace",
+                image: "image/necklace.jpg",
+                specs: "Includes top,pant and basic stationery",
+                weight: ["Standard", "Premium"],
+                price: {"Standard": "₹115", "Premium": "₹5000"},
+                shops: ["sports shop", "abc sports"]
+            },
+            {
+                name: "rings",
+                image: "image/rings.jpg",
+                specs: "Includes belt, and basic stationery",
+                weight: ["Standard", "Premium"],
+                price: {"Standard": "₹150", "Premium": "₹500"},
+                shops: ["sports shop", "abc shop"]
+            },
+            {
+                name: "bracelate",
+                image: "image/bracelate.jpg",
+                specs: "Includes pant top and basic stationery",
+                weight: ["Standard", "Premium"],
+                price: {"Standard": "₹600", "Premium": "₹1500"},
+                shops: ["lulu", "abc shop"]
+            },
+            {
+                name: "earrings",
+                image: "image/earrings.jpg",
+                specs: "Includes pant top and basic stationery",
+                weight: ["Standard", "Premium"],
+                price: {"Standard": "₹600", "Premium": "₹1500"},
+                shops: ["lulu", "abc shop"]
+            },
+        ],
+            furniture: [
+                {
+                    name: "sofa",
+                    image: "image/sofa.jpg",
+                    specs: "Includes top,pant and basic stationery",
+                    weight: ["Standard", "Premium"],
+                    price: {"Standard": "₹10115", "Premium": "₹14000"},
+                    shops: ["sports shop", "abc sports"]
+                },
+                {
+                    name: "coffe table",
+                    image: "image/coffe table.jpg",
+                    specs: "Includes belt, and basic stationery",
+                    weight: ["Standard", "Premium"],
+                    price: {"Standard": "₹1500", "Premium": "₹5000"},
+                    shops: ["sports shop", "abc shop"]
+                },
+                {
+                    name: "bean bag",
+                    image: "image/bean bag.jpg",
+                    specs: "Includes pant top and basic stationery",
+                    weight: ["Standard", "Premium"],
+                    price: {"Standard": "₹7000", "Premium": "₹18000"},
+                    shops: ["lulu", "abc shop"]
+                },
+                {
+                    name: "tv stand",
+                    image: "image/tv stand.jpg",
+                    specs: "Includes pant top and basic stationery",
+                    weight: ["Standard", "Premium"],
+                    price: {"Standard": "₹6000", "Premium": "₹15000"},
+                    shops: ["lulu", "abc shop"]
+                },
+            
+         ],
+         makeupproducts: [
+            {
+                name: "primer",
+                image: "image/primer.jpg",
+                specs: "Includes top,pant and basic stationery",
+                weight: ["Standard", "Premium"],
+                price: {"Standard": "₹10115", "Premium": "₹14000"},
+                shops: ["sports shop", "abc sports"]
+            },
+            {
+                name: "loose powder",
+                image: "image/loose powder.jpg",
+                specs: "Includes belt, and basic stationery",
+                weight: ["Standard", "Premium"],
+                price: {"Standard": "₹1500", "Premium": "₹5000"},
+                shops: ["sports shop", "abc shop"]
+            },
+            {
+                name: "blush",
+                image: "image/blush.jpg",
+                specs: "Includes pant top and basic stationery",
+                weight: ["Standard", "Premium"],
+                price: {"Standard": "₹7000", "Premium": "₹18000"},
+                shops: ["lulu", "abc shop"]
+            },
+            {
+                name: "foundation",
+                image: "image/foundation.jpg",
+                specs: "Includes pant top and basic stationery",
+                weight: ["Standard", "Premium"],
+                price: {"Standard": "₹6000", "Premium": "₹15000"},
+                shops: ["lulu", "abc shop"]
+            },
+            {
+                name: "setting spray",
+                image: "image/setting spray.jpg",
+                specs: "Includes pant top and basic stationery",
+                weight: ["Standard", "Premium"],
+                price: {"Standard": "₹6000", "Premium": "₹15000"},
+                shops: ["lulu", "abc shop"]
+            },
+        
+     ],
 };
 
 let currentCategory = '';
